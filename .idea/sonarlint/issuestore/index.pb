6f5316449a82718f1a61bf9e9b7512bdcdf8bd41
Aryan Chauhan <aryan.chauhan@pkware.com>
2024-02-18 00:00:18 +0530

    Initial Commit

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,5 @@
+
+7
+pom.xml,4/4/442292b8a7efeabbe4cc176709b833b1792140ec
+
+Osrc/main/java/com/ticketbooking/serviceRegistry/ServiceRegistryApplication.java,7/3/733d2c271a187ce9f0b9bcce746927344d98ca75
---
+++ src/test/java/com/ticketbooking/serviceRegistry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ticketbooking.serviceRegistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
